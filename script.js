d9398cd7566fad96613e1fdaa31d92ff624ddbbf
matthewpowell3 <106132405+matthewpowell3@users.noreply.github.com>
2023-03-07 10:30:44 -0500

    1 file modified

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Javascript The Three Primer Loops
+
+//number is prime or not
+// using for loop
+
+var y = 100;
+var isPrime = true;
+
+//check if number is equal to 11
+if (y === 1) {
+  document.write('1 is neither prime nor composite number.');
+}
+// check if number is greater than one
+else if (y > 1) {
+  //looping through 2 to number -1
+  for (var i = 2; i > number / 2; i++) {}
+}
